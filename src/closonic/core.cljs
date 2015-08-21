2c1e5670d38cf7eed35f5d165c38668aeb0e9a6e
John Krasnay <john@krasnay.ca>
2015-08-20 23:56:20 -0400

    Some interactivity and control functions

--- src/closonic/core.cljs
+++ src/closonic/core.cljs
@@ -9,45 +9,63 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state
-  (atom {:playlist [{:track "Pillar of Salt" :artist "The Thermals" :album "The Body, the Blood, the Machine"}
-                    {:track "Driver 8" :artist "REM" :album "Eponymous" }
-                    {:track "Alone+Easy Target" :artist "Foo Fighters" :album "Foo Fighters" }]
-         :current 0}))
+  (atom {:playlist [{:id 1 :track "Pillar of Salt" :artist "The Thermals" :album "The Body, the Blood, the Machine"}
+                    {:id 2 :track "Driver 8" :artist "REM" :album "Eponymous" }
+                    {:id 3 :track "Alone+Easy Target" :artist "Foo Fighters" :album "Foo Fighters" }]
+         :current 2}))
+
+;; control functions
+;;
+
+(defn play-song [index]
+  "Plays the song with the given ID. The song must already be in the playlist.
+   Not sure, but it seems we should pass the app-state atom in here."
+  (swap! app-state assoc :current index))
+
+(defn play-next []
+  (play-song (min (inc (:current @app-state)) (dec (count (:playlist @app-state))))))
+
+(defn play-prev []
+  (play-song (max (dec (:current @app-state)) 0)))
+
+
+;; UI components
+;;
 
 (defn playlist-item [item owner]
-  "Renders a single item in the playlist"
+  "Renders a single item in the playlist. Item is a map with :data holding the app state and :index holding the index of this song"
   (reify
     om/IRender
     (render [_]
-      (dom/li #js {:className "Playlist-item"}
-              (str (:track item) " - " (:artist item))))))
+      (let [song ((:playlist (:data item)) (:index item))]
+        (dom/li #js {:className (str "Playlist-item" (when (= (:index item) (:current (:data item))) " is-playing"))
+                     :onClick (fn [_] (play-song (:index item)))}
+                (str (:track song) " - " (:artist song)))))))
 
 (defn playlist [data owner]
   "Renders the playlist below the 'currently playing' bit."
   (reify
     om/IRender
     (render [_]
-      (println "render playlist" data)
       (apply dom/ul #js {:className "Playlist"}
-             (om/build-all playlist-item (:playlist data))))))
+             (om/build-all playlist-item (map #(hash-map :index % :data data) (range (count (:playlist data)))))))))
 
 (defn now-playing [data owner]
   "Renders the 'now playing' block. Data is the entire app data"
   (reify
     om/IRender
     (render [_]
-      (let [current ((:playlist data) (:current data))]
-      (dom/div #js {:className "NowPlaying"}
-               (dom/div #js {:className "NowPlaying-track"} (:track current))
-               (dom/div #js {:className "NowPlaying-artist"} (:artist current))
-               (dom/div #js {:className "NowPlaying-album"} (:album current)))))))
+      (let [song ((:playlist data) (:current data))]
+        (dom/div #js {:className "NowPlaying"}
+                 (dom/div #js {:className "NowPlaying-track"} (:track song))
+                 (dom/div #js {:className "NowPlaying-artist"} (:artist song))
+                 (dom/div #js {:className "NowPlaying-album"} (:album song)))))))
 
 (defn playlist-view [data owner]
   "Renders the playlist view. Data is the entire app state"
   (reify
     om/IRender
     (render [_]
-      (println "render playlist view" (:playlist data))
       (dom/div #js {:className "PlaylistView"}
                (om/build now-playing data)
                (om/build playlist data)))))
